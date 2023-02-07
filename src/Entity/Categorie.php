80b681783991024dbd28cd24d935b6b375ab19fd
Benjamin Lemire <benlemire@hotmail.ca>
2023-02-07 16:59:51 -0500

    creation de la base de donnee

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[ORM\Table(name: 'categories')]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'idCategorie')]
+    private ?int $idCategorie = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $categorie = null;
+
+    public function getIdCategorie(): ?int
+    {
+        return $this->idCategorie;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Produit.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+#[ORM\Table(name: 'produits')]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'idProduit')]
+    private ?int $idProduit = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(nullable: true, name: 'quantiteEnStock')]
+    private ?int $quantiteEnStock = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 100, name: 'imagePath')]
+    private ?string $imagePath = null;
+
+
+    public function getIdProduit(): ?int
+    {
+        return $this->idProduit;
+    }
+
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantiteEnStock(): ?int
+    {
+        return $this->quantiteEnStock;
+    }
+
+    public function setQuantiteEnStock(?int $quantiteEnStock): self
+    {
+        $this->quantiteEnStock = $quantiteEnStock;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(string $imagePath): self
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+}
